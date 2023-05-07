7f6ae08c700b65d260148e7ddf24413f7bd39bd3
Proprogam <k.kartal9595@gmail.com>
2023-05-08 00:36:22 +0300

    renewed

--- src/FlashCard.js
+++ src/FlashCard.js
@@ -7,42 +7,37 @@ export default function FlashCard({ flashcard }) {
 
   return (
     <div className="card">
-      <div className="layer"></div>
-
-      <div className="content">
-        <div className="details">
-          <div className="image">
-            <img className="img" src={flashcard.img} alt="" />
-          </div>
-          <div
-            onClick={() => {
-              const audio = new Audio(flashcard.sound);
-              audio.play();
-            }}
-            className="buton-1"
-          >
-            <div className="title-1">{flashcard.title}</div>
-            <FileMusic className="icon-1" size={56} />
-          </div>
-          <div onClick={() => setFlip(!flip)} className="buton-2">
-            <div className="title-2">
-              {flip ? flashcard.meaning : flashcard.title}
-            </div>
-            <ArrowRepeat className="icon-2" size={36} />
-          </div>
+      <div className="details">
+        <div className="image">
+          <img src={flashcard.img} alt="" />
         </div>
-
-        <div className="bottom">
-          <div className="first-text">
-            <h3>{flashcard.ex1}</h3>
-            <h2>{flashcard.ex1mng}</h2>
-          </div>
-          <div className="second-text">
-            <h3>{flashcard.ex2}</h3>
-            <h2>{flashcard.ex2mng}</h2>
-          </div>
+        <div
+          onClick={() => {
+            const audio = new Audio(flashcard.sound);
+            audio.play();
+          }}
+          className="buton-1"
+        >
+          <div className="title-1">{flashcard.title}</div>
+          <FileMusic className="icon-1" size={56} />
         </div>
-        <div></div>
+        <div onClick={() => setFlip(!flip)} className="buton-2">
+          <div className="title-2">
+            {flip ? flashcard.meaning : flashcard.title}
+          </div>
+          <ArrowRepeat className="icon-2" size={36} />
+        </div>
+      </div>
+
+      <div className="bottom">
+        <div className="first-text">
+          <h3>{flashcard.ex1}</h3>
+          <h2>{flashcard.ex1mng}</h2>
+        </div>
+        <div className="second-text">
+          <h3>{flashcard.ex2}</h3>
+          <h2>{flashcard.ex2mng}</h2>
+        </div>
       </div>
     </div>
   );
